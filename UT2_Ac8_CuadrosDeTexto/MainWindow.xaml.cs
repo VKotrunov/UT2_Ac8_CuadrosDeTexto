eedf32d1fdff0464e0b80faf69bfcd1b9310a47f
VKotrunov <57618856+VKotrunov@users.noreply.github.com>
2020-10-20 13:59:16 +0200

    Agregar archivos de proyecto.

---
+++ UT2_Ac8_CuadrosDeTexto/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace UT2_Ac8_CuadrosDeTexto
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void edadTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F2) {
+                bool verificacion = int.TryParse(edadTextBox.Text, out int edad);
+                if (!verificacion)
+                    edadTextBlock.Text = "Edad incorrecta";
+                else
+                    edadTextBlock.Text = "";
+            }
+        }
+
+        private void nombreTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F1 && nombreTextBlock.Text == "")
+            {
+                nombreTextBlock.Text = "Nombre del cliente";
+            }
+            else
+                nombreTextBlock.Text = "";
+        }
+
+        private void apellidoTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F1 && apellidoTextBlock.Text == "")
+            {
+                apellidoTextBlock.Text = "Apellido del cliente";
+            }
+            else
+                apellidoTextBlock.Text = "";
+        }
+    }
+}
